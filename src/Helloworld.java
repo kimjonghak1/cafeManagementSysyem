9bd4edc57ca670ad0e23b1387247aa2cf9de3fd1
종학 <@DESKTOP-7AQG85C>
2020-03-17 21:25:42 +0900

    Create a menu

---
+++ src/Helloworld.java
@@ -0,0 +1,17 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*** Cafe Management System Menu ***");
+		System.out.println("Select one number between 1 - 5:");
+		System.out.println("1. Add Cafe");
+		System.out.println("2. Delete Cafe");
+		System.out.println("3. Edit Cafe");
+		System.out.println("4. View Cafe");
+		System.out.println("5. Exit");
+		
+		
+	}
+
+}
